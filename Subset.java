442b57066b00a72acd0a656257f6e165f84f2cf8
congxiu <congxiu@Congs-MacBook-Pro.local>
2015-09-27 23:36:34 -0400

    some data structures...

---
+++ Subset.java
@@ -0,0 +1,22 @@
+/*
+ * A string sampler
+ */
+
+public class Subset
+{
+    public static void main(String[] args)
+    {
+        String currIn;
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> randQ = new RandomizedQueue<String>();
+        
+        while (!StdIn.isEmpty())
+        {
+            currIn = StdIn.readString();
+            randQ.enqueue(currIn);
+        }
+        
+        for (int i = 0; i < k; i++)
+            StdOut.println(randQ.dequeue());
+    }
+}
